2d580aad367aa2c8296013bb9624852e859a3702
Jacqueline McCleary <jmac.ftw@gmail.com>
2023-08-25 14:48:57 -0400

    rearranged cat_prep_runner

--- runner_scripts/cat_prep_runner.py
+++ runner_scripts/cat_prep_runner.py
@@ -6,8 +6,8 @@ from src.catalog import Catalog
 from src.hpmask import HpMask
 from src.cat_utils import cat_config_checker
 
-#config = utils.read_yaml('prep_catalog_config.yaml')
-config = utils.read_yaml('configs/prep_hidens_randoms_config.yaml')
+config = utils.read_yaml('../configs/prep_catalog_config.yaml')
+#config = utils.read_yaml('configs/prep_hidens_randoms_config.yaml')
 
 overwrite = True
 vb = True
@@ -19,43 +19,36 @@ vb = True
 
 # maybe a little shady to do this? Ideally, would happen
 # somewhere inside Catalog.
+
 config = cat_config_checker(config)
 
 if not os.path.isdir(config['paths']['output_path']):
     os.system(f'mkdir {config["paths"]["output_path"]}')
 
-#run_config = read_yaml(args.config)
+# Load foreground catalog from configuration file
+fg = Catalog(config=config['foreground_catalog'], vb=vb)
 
-# Try loading catalog from the configuration file.
+# Create its mask
+fg.create_mask_from_config(mask_config=config['foreground_mask'])
+
+# Apply mask to foregound catalog & save 
+fg.apply_mask()
+
+# Load background catalog from the configuration file.
 bg_redshift = Catalog(config=config['background_catalog'], vb=vb)
 
 # Load mask from config -- TO DO: add error checking!
 bg_redshift.create_mask_from_config(mask_config=config['background_mask'])
 
-# Load other catalog from configuration file
-#fg = Catalog(config=config['foreground_catalog'], vb=vb)
-
-# Create its mask
-#fg.create_mask_from_config(mask_config=config['foreground_mask'])
-
-# Instatiate background mask
-filepath =  os.path.join(config['foreground_mask']['path'], \
-                            config['foreground_mask']['filename'])
-coordframe = config['foreground_mask']['coordframe']
-
-fg_mask = HpMask(filepath=filepath, coordframe=coordframe)
-
-
 # Find overlapping masks
-bg_redshift.apply_overlapping_masks(mask1=bg_redshift.mask, mask2=fg_mask)
-
+bg_redshift.apply_overlapping_masks(mask1=bg_redshift.mask, mask2=fg.mask)
 
 start = time.time()
 
 # Join if requested
 if 'match' in config['background_catalog'].keys():
-    match_cat = Catalog(config['background_catalog']['match'])
+    match_cat = Catalog(config['background_catalog']['match'], memmap=True)
     bg_redshift.match_to_catalog(match_cat, overwrite=overwrite)
 
 end = time.time()
-print(f"\n Random matching took {end-start} ms \n")
+print(f"\n Random matching took {(end-start)/1.0e3} s \n")
