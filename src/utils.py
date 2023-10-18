4fc2ff1476e74d7a15ae386ceb79f008163e8996
mcclearyj <jmac.ftw@gmail.com>
2023-10-18 15:04:17 -0400

    bugfix: index cat2 with correct ra/dec tag

--- src/utils.py
+++ src/utils.py
@@ -97,14 +97,15 @@ def match_coords(cat1, cat2, ratag1=None, dectag1=None,
             cat1_ra = cat1['ALPHAWIN_J2000']
             cat1_dec =  cat1['DELTAWIN_J2000']
         else:
-            raise KeyError('cat1: no "ra,dec" or "{ALPHA,DELTA}WIN_J2000" columns')
+            raise KeyError('cat1: no "ra,dec" or ',
+                           '"{ALPHA,DELTA}WIN_J2000" columns')
     except:
-        print("Couldn't load catalog 1 RA & Dec")
+        raise NameError("\nCouldn't load catalog 1 RA & Dec\n")
 
     try:
         if (ratag2 is not None) and (dectag2 is not None):
-            cat2_ra = cat2[ratag1]
-            cat2_dec =  cat2[dectag1]
+            cat2_ra = cat2[ratag2]
+            cat2_dec =  cat2[dectag2]
         elif 'ra' in cat2.colnames:
             cat2_ra = cat2['ra']
             cat2_dec =  cat2['dec']
@@ -112,9 +113,10 @@ def match_coords(cat1, cat2, ratag1=None, dectag1=None,
             cat2_ra = cat2['ALPHAWIN_J2000']
             cat2_dec =  cat2['DELTAWIN_J2000']
         else:
-            raise KeyError('cat2: no "ra,dec" or "{ALPHA,DELTA}WIN_J2000" columns')
+            raise KeyError('cat2: no "ra,dec" or ',
+                           '"{ALPHA,DELTA}WIN_J2000" columns')
     except:
-        print("Couldn't load catalog 2 RA & Dec")
+        raise NameError("\nCouldn't load catalog 2 RA & Dec\n")
 
     cat1_matcher = htm.Matcher(16, ra=cat1_ra, dec=cat1_dec)
 
