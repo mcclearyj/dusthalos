fbaf061f4787a5a6ad15917123cff934cdcf8fb4
Jacqueline McCleary <jmac.ftw@gmail.com>
2023-11-21 16:55:15 -0500

    add sample hpc batch script

---
+++ job_dust.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+#SBATCH -t 00:59:59
+#SBATCH -N 1
+#SBATCH --mem=50G
+#SBATCH --partition=express
+#SBATCH -J dust_cat_maker
+#SBATCH -v
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jmac.ftw@gmail.com
+#SBATCH -o slurm-dust_cat_maker.out
+
+
+###
+### Define some environmental variables
+###
+
+export CODEDIR='/work/mccleary_group/dusty_halos/dusthalos'
+export CONFIGDIR='/work/mccleary_group/dusty_halos/dusthalos/configs'
+export PATH=$PATH:'/work/mccleary_group/Software/texlive-bin/x86_64-linux'
+
+echo $PATH
+echo $PYTHONPATH
+echo $CONFIGDIR
+
+dirname="slurm_outfiles"
+if [ ! -d "$dirname" ]
+then
+     echo " Directory $dirname does not exist. Creating now"
+     mkdir -p -- "$dirname"
+     echo " $dirname created"
+ else
+     echo " Directory $dirname exists"
+ fi
+
+ echo "Proceeding with code..."
+
+###
+### Activate Conda
+### 
+source /work/mccleary_group/miniconda3/etc/profile.d/conda.sh
+
+conda activate dustyhalos
+
+
+###
+### Go!
+###
+
+python $CODEDIR/runner_scripts/dust_calc_runner.py --config_file $CONFIGDIR/dust_calc_config_lowz_hidens.yaml
+
+
+#mv slurm-dust_cat_maker.out "$dirname"
+
