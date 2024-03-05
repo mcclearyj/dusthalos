7d9aa4134ed72c5d483d7f3d3da184b3707fd95a
mcclearyj <jmac.ftw@gmail.com>
2024-03-04 21:37:34 -0700

    update default nrand

--- runner_scripts/make_overlap_plots.py
+++ runner_scripts/make_overlap_plots.py
@@ -7,13 +7,12 @@ out_dir = '/Users/j.mccleary/Research/dusty_halos/catalogs/prep_cat_output'
 #cat_dir = '/work/mccleary_group/dusty_halos/prep_cat_output'
 #cat_dir = '/work/mccleary_group/dusty_halos/prep_cat_output'
 
-cat1_basename = 'prep_cat_output/DoubleMasked_gaia_desdr2_overlap_full.fits'
-cat2_basename = 'prep_cat_output/redmagic_hiz_y3_GOLD_JOINED_catalog.fits'
+cat1_basename = 'prep_cat_output/DoubleMasked_sdss_bg_randoms.fits'
+cat2_basename = 'prep_cat_output/DoubleMasked_sdss_bg_gals.fits'
 cat3_basename = 'gaia_desdr2_overlap_full.fits'
 
-cat1_label = 'Masked Gaia stars'
-cat2_label = 'redMaGiC hi-lum hi-z'
-cat3_label = 'Gaia stars'
+cat1_label = 'Randoms'
+cat2_label = 'Galaxies'
 
 cat1 = os.path.join(cat_dir, cat1_basename)
 cat2 = os.path.join(cat_dir, cat2_basename)
@@ -22,16 +21,16 @@ cat3 = os.path.join(cat_dir, cat3_basename)
 op = OverlapPlotter(cat1_name=cat2, cat2_name=cat1)
 op2 = OverlapPlotter(cat1_name=cat3, cat2_name=cat2)
 
-outname = os.path.join(out_dir, 'masked_gaia_redmagic_overlap_rectilin.pdf')
+outname = os.path.join(out_dir, 'masked_sdss_bg_overlap_rectilin.pdf')
 op.make_plot(outname=outname, label1=cat2_label, coordframe1='icrs',
              label2=cat1_label, coordframe2='icrs',
-             ra_key1='ra_redmagic_hiz', dec_key1='dec_redmagic_hiz'
+             ra_key1='ra_rand', dec_key1='dec_rand'
              )
 
-outname = os.path.join(out_dir, 'masked_gaia_redmagic_overlap_aitoff.pdf')
+outname = os.path.join(out_dir, 'masked_sdss_bg_overlap_aitoff.pdf')
 op.make_plot(outname=outname, projection='aitoff', label1=cat2_label,
              coordframe1='icrs', label2=cat1_label, coordframe2='icrs',
-             ra_key1='ra_redmagic_hiz', dec_key1='dec_redmagic_hiz'
+             ra_key1='ra_rand', dec_key1='dec_rand'
              )
 '''
 outname2 = os.path.join(out_dir, 'gaia_redmagic_overlap_rectilin.pdf')
