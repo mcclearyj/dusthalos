d9bf3d93471e8ad715e3a561458dd0be4be6f2f5
mcclearyj <jmac.ftw@gmail.com>
2024-10-24 08:16:33 -0400

    not csfd

--- src/do_wls_fit.py
+++ src/do_wls_fit.py
@@ -1,3 +1,5 @@
+import statsmodels.api as sm
+
 def do_WLS_fit(this_dk, dust_rel, name=None, start=None, end=None, log=False):
     """
     Perform a weighted least-squares linear regression to
@@ -12,23 +14,29 @@ def do_WLS_fit(this_dk, dust_rel, name=None, start=None, end=None, log=False):
         log: if True, fit the relationship in log-space to stabilize the fits.
              Default is False.
     """
-
+    dust_rel = dust_rel[start:end]
+    this_dk = this_dk[start:end]
     ## Transform the dependent variable (fully compensated kappa)
     if log == True:
-        Y = np.log(dust_rel[start:end])
+        # Keep positive values only...
+        ok = (dust_rel > 0)
+        dust_rel = dust_rel[ok]
+        this_dk = this_dk[ok]
+        # Fit log of dust relation
+        Y = np.log(dust_rel)
         ## Also need to transform the weights!
-        weights_OG = 1/(this_dk['sigma'][start:end]**2)
+        weights_OG = 1/(this_dk['sigma']**2)
         weight_scale = (1/Y)**2
         weights = weights_OG * weight_scale
 
     ## Just do regular fit
     else:
-        Y = dust_rel[start:end]
-        weights = 1/(this_dk['sigma'][start:end]**2)
+        Y = dust_rel
+        weights = 1/(this_dk['sigma']**2)
 
     ## If plotting X on a log-scale, which we are,
     ## fit relationship on log scale
-    X = this_dk['meanlogr'][start:end]
+    X = this_dk['meanlogr']
 
     ## Add intercept to abscissa
     X = sm.add_constant(X)
